47b63a40c2ff6e3efcbe06cdc43273d864ced8e1
pkodama <kodama.subu@gmail.com>
2019-04-02 06:20:51 +0000

    Inital commit

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models/count.rb'
+
+before do
+  if Count.all.size == 0
+    Count.create(number: 0)
+  end
+end
+
+get '/' do
+  'こんにちは'
+end
+
+get '/count' do
+  @number = Count.first.number
+  erb :index
+end
+
+post '/plus' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect '/count'
+end
+
+post '/minus' do
+  count = Count.first
+  count.number = count.number - 1
+  count.save
+  redirect '/count'
+end
+
+post '/times' do
+  count = Count.first
+  count.number = count.number*2
+  count.save
+  redirect '/count'
+end
+
+post '/devide' do
+  count = Count.first
+  count.number = count.number/2
+  count.save
+  redirect '/count'
+end
